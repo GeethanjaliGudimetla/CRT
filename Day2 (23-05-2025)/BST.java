d92b229844bf1321340df4706bb77ee55227e5f3
GeethanjaliGudimetla <geethanjalig0508@gmail.com>
2025-05-29 15:10:06 +0530

    Binary Search Tree

---
+++ Day2 (23-05-2025)/BST.java
@@ -0,0 +1,38 @@
+class Node{
+    int key;
+    Node left,right;
+    public Node(int item){
+        key=item;
+        left=right=null;
+    }
+}
+class BST{
+    static Node insert(Node root,int key){
+        if(root==null){return new Node(key);}
+        if(root.key==key){ return root;}
+        if(key<root.key)
+        root.left=insert(root.left,key);
+        else
+        root.right=insert(root.right,key);
+        return root;
+    }
+    static void inorder(Node root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.println(root.key+" ");
+            inorder(root.right);
+        }
+
+    }
+    public static void main(String[] args) {
+        Node root=null;
+        root=insert(root, 50);
+        root=insert(root, 30);
+        root=insert(root, 20);
+        root=insert(root, 70);
+        root=insert(root, 60);
+        root=insert(root, 80);
+        inorder(root);
+
+    }
+}
